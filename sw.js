0c998593d6302a9ffea6a3c3e2fac043907f4684
gamache61 <truckman.remi@gmail.com>
2025-12-21 17:45:29 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'appaudit-guardian-v4.9.2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install: Cache the stable v4.9.2 assets
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate: Purge any remnants of v4.9.5 or other versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch: Standard strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
