cd08911ec88e32c22426a4b79561262b24d2bcd2
gamache61 <truckman.remi@gmail.com>
2025-12-23 16:14:07 -0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const prompt = `Perform a high-precision HTML structural audit. 
-Check for:
-1. Unclosed tags (like <p>, <h2>, <div>).
-2. Incorrect nesting (like <b><p></b>).
-3. Malformed tags (like <img src="" ).
-
-If the code has ANY of these technical flaws, list them. 
-If and ONLY if the code is 100% syntactically perfect, return [].
-Code: ${code}`;
